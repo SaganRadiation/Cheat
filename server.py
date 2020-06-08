e688872880e67acb7dc602cc40ff53e674ec6f09
saganradiation <jakegrimes9@gmail.com>
2020-06-08 19:26:47 -0400

    Revert "cors stuff"
    This reverts commit ba32b6d93926c611af1f41a55c5288cb8888af87.

--- server.py
+++ server.py
@@ -1,18 +1,15 @@
 import logging
 import os
 from flask import Flask, render_template
-from flask_cors import CORS, cross_origin
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.debug = 'DEBUG' in os.environ
-CORS(app)
 socketio = SocketIO(app)
 
 messages = []
 
 @app.route('/')
-@cross_origin()
 def hello():
   return render_template('index.html')
 
