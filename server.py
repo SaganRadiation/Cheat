051e7a9800b99a6da529f541e73e69f73321eb27
saganradiation <jakegrimes9@gmail.com>
2020-06-08 19:53:28 -0400

    replace the messages with sids

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 import logging
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.debug = 'DEBUG' in os.environ
 socketio = SocketIO(app)
 
-messages = []
+sids= []
 
 @app.route('/')
 def hello():
@@ -15,12 +15,12 @@ def hello():
 
 @socketio.on('connect')
 def connect():
-  emit('my response',  {'messages': messages})
+  emit('my response',  {'messages': sids})
 
 @socketio.on('add message')
 def broadcast(message):
-  messages.append(message['data'])
-  emit('my response', {'messages': messages}, broadcast=True)
+  sids.append(request.sid)
+  emit('my response', {'messages': sids}, broadcast=True)
 
 if __name__ == '__main__':
   socketio.run(app)
